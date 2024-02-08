2ee034bb51cf909e4a7ec74769e7a0080777ee97
Soukayna Alala <soukayna.alala45@gmail.com>
2024-02-08 19:10:16 +0000

    removed some leftover vite boilerplate code and added star wars background image

--- src/components/StarShip/StarShip.tsx
+++ src/components/StarShip/StarShip.tsx
@@ -1,13 +1,24 @@
 import Styles from "./StarShip.module.css";
 import { Starship } from "./interfaces.ts";
 
-export function StarShip({ name, passengers }: Starship) {
+export function StarShip({ name, passengers, crew, films }: Starship) {
   const { listItem } = Styles;
 
   return (
     <li className={listItem}>
-      <div>name: {name}</div>
-      <div>passengers: {passengers}</div>
+      <div>
+        <strong>Passengers:</strong> {name}
+      </div>
+      <div>
+        <strong>Passengers:</strong> {passengers}
+      </div>
+      <div>
+        <strong>Crew:</strong> {crew}
+      </div>
+      <div>
+        <strong>Film Count: </strong>
+        {films.length}
+      </div>
     </li>
   );
 }
--- src/components/StarShipList/StarShipList.tsx
+++ src/components/StarShipList/StarShipList.tsx
@@ -6,29 +6,25 @@ import { StarshipsResponse } from "./interfaces.ts";
 import { url } from "../StarShip/constants.ts";
 
 export function StarShipList() {
-  console.log("%c Component rendered ", "background:red; color:white");
   const [starships, setStarships] = useState<StarshipsResponse | null>(null);
 
   useEffect(() => {
     fetch(url)
       .then((response) => response.json())
-      .then((data: StarshipsResponse) => {
-        console.log("I HAVE THE DATA", data);
-        setStarships(data);
-      });
+      .then((data: StarshipsResponse) => setStarships(data));
   }, []);
 
-  const { listOfNames } = Styles;
+  const { list, loading } = Styles;
 
   return (
     <div>
       {starships === null ? (
-        "...Loading"
+        <h1 className={loading}>...Loading</h1>
       ) : (
-        <ul className={listOfNames}>
-          {starships.results.map((i) => {
-            return <StarShip {...i} key={i.name} />;
-          })}
+        <ul className={list}>
+          {starships.results.map((i) => (
+            <StarShip {...i} key={i.name} />
+          ))}
         </ul>
       )}
     </div>
